1983d799eef99e974fe44c191a6bdc8b8b73bcc8
Roshu-netizen <rohaanali687@gmail.com>
2024-07-12 22:14:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const target = document.getElementById("target");
+let yes = document.getElementById("yes");
+
+yes.addEventListener("click", () => {
+  alert("I Love You Too Jaanuu💗💕");
+});
+
+
+target.addEventListener("click",()=>{
+alert('Chal Hatt ! Yes Pr Duba 😡')
+})
